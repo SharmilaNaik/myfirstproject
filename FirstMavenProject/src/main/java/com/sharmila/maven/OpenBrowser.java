7741b709579296cbff648378a4d597abfc42112d
Sharmila Naik <sagar20688@gmail.com>
2022-11-10 10:58:51 +0530

    committing after changing username

--- FirstMavenProject/src/main/java/com/sharmila/maven/OpenBrowser.java
+++ FirstMavenProject/src/main/java/com/sharmila/maven/OpenBrowser.java
@@ -7,7 +7,7 @@ public class OpenBrowser {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","D:\\JARs\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		ChromeDriver driver= new ChromeDriver();
-		driver.get("https://qa-cnc.rameesystems.com/"); 
+		driver.get("https://www.google.com"); 
 		String url=driver.getCurrentUrl();
 		System.out.println(url);
 	}
