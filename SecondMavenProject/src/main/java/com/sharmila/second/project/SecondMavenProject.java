a62c956b206529aa5c18ac25c0a266ddd0b6c0bf
Sharmila Naik <sharmilamadane717@gmail.com>
2022-11-10 11:18:48 +0530

    this is second commit

---
+++ SecondMavenProject/src/main/java/com/sharmila/second/project/SecondMavenProject.java
@@ -0,0 +1,16 @@
+package com.sharmila.second.project;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondMavenProject {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\JARs\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/"); 
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+
+	}
+
+}
