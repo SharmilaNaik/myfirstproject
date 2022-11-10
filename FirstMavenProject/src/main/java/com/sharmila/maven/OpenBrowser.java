e14fa4510670b4facc9b89495205c8f54e2d1f77
Sagar Naik <sagar20688@gmail.com>
2022-11-10 10:42:23 +0530

    Commiting FirstMavenProject

---
+++ FirstMavenProject/src/main/java/com/sharmila/maven/OpenBrowser.java
@@ -0,0 +1,16 @@
+package com.sharmila.maven;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\JARs\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://qa-cnc.rameesystems.com/"); 
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+	}
+
+}
+
